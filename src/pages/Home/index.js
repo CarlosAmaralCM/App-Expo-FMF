ddd73c97b717212f67c687800f02e928c100fe91
CarlosAmaralCM <carlos.amaral.dev@outlook.com>
2024-05-14 13:15:18 -0400

    botao switch map adicionado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 import Header from '../../components/Header'
 
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 55 : 64;
+
 export default function Home() {
+  const [selectedTab, setSelectedTab]=useState(0);
   return (
     <ScrollView  style={styles.container}>
-      <View>
-          <Header>
-          </Header>
+      <View style={styles.header}>
+        <View style={styles.subHeader}>
+          <View style={styles.switch}>
+            <TouchableOpacity style={{width: '50%',
+            height: 30,
+            backgroundColor: selectedTab == 0 ? '#FF7A00' : 'white',
+            borderRadius: 15,
+            justifyContent: 'center',
+            alignItems: 'center'}}
+            onPress={() => {
+              setSelectedTab(0);
+            }}> 
+              <Text style={styles.subCent}> Lista</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{width: '50%',
+            height: 30,
+            backgroundColor: selectedTab == 1 ? '#FF7A00' : 'white',
+            borderRadius: 15,
+            justifyContent: 'center',
+            alignItems: 'center'}}
+            onPress={() => {
+              setSelectedTab(1);
+            }}>
+              <Text style={styles.subCent}> Mapa</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
 
       <View style={styles.subContainer}>
@@ -54,10 +82,11 @@ const styles = StyleSheet.create({
   },
   subCent: {
     fontSize:14,
-    //fontWeight: 'bold',
+    fontWeight: 'bold',
     color: '#A6A6A6'
   },
   subContainer: {
+    paddingTop: 10,
     alignSelf:'center',
     flex:1,
     width: '100%',
@@ -79,5 +108,44 @@ const styles = StyleSheet.create({
     height: 500,
     backgroundColor:'#FF7A00'
   },
+  header:{
+    backgroundColor: '#043B74',
+    paddingTop: statusBarHeight,
+    //alignItems: 'center',
+    //justifyContent: 'center'
+  },
+  subHeader:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  switch:{
+    width:'30%',
+    height: 35,
+    borderWidth: 0.5,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 15,
+    flexDirection:'row',
+    alignItems: 'center',
+    paddingLeft: 3,
+    paddingRight: 3,
+    
+  },
+  /*touch1:{
+    width: '50%',
+    height: 60,
+    backgroundColor: selectedTab == 0 ? 'blue' : 'white',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  touch2:{
+    width: '50%',
+    height: 60,
+    backgroundColor: selectedTab == 1 ? 'white' : 'blue',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }, */
 
 });
